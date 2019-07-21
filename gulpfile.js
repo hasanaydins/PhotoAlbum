57b3d3ce2363428aa506963d9ef2e0242e5458f0
Hasan Aydın <hasanaydins@gmail.com>
2019-07-21 04:41:24 +0300

    Arayüz düzenlemesi logout ve diğer düzenlemeler

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,34 @@
-const { src, dest, parallel } = require('gulp');
+const { src, dest,series,watch } = require('gulp');
 const less = require('gulp-less');
 const minifyCSS = require('gulp-csso');
 const uglify = require('gulp-minify');
 
-
 function css() {
-  return src('static/less/*.less')
-      .pipe(less())
-      .pipe(minifyCSS())
-      .pipe(dest('dist/css'))
+    return src('static/less/*.less')
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(dest('dist/css'))
 }
 
 function minify() {
-  return src('static/js/*.js', { sourcemaps: true })
-  // Minify the file
-      .pipe(uglify())
-      // Output
-      .pipe(dest('dist/js', { sourcemaps: true }))
+    return src('static/js/*.js', { sourcemaps: true })
+    // Minify the file
+        .pipe(uglify())
+        // Output
+        .pipe(dest('dist/js', { sourcemaps: true }))
+}
+
+function watcher(done) {
+    // css changes
+    watch('static/**/*.less', css);
+    // js changes
+    watch('static/**/*.js', minify);
+    done();
 }
 
 
+exports.watcher = watcher;
 exports.css = css;
 exports.minify = minify;
 
-exports.default = parallel(css, minify);
+exports.default = series(exports.watcher, exports.css, exports.minify);
